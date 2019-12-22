2ce3876024d72165471f6665605f4f6d3a4b94d5
elfi yang <elfiyang16@gmail.com>
2019-12-22 10:40:17 +0000

    feat: add rendering NYT articles

--- src/script.js
+++ src/script.js
@@ -1,22 +1,54 @@
 
 function loadData() {
-    let $body = $('body');
-    let $wikiElem = $('#wikipedia-links');
-    let $nytHeaderElem = $('#nytimes-header');
-    let $nytElem = $('#nytimes-articles');
-    let $greeting = $('#greeting');
+    var $body = $('body');
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var $greeting = $('#greeting');
 
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
 
-    // YOUR CODE GOES HERE!
+    // let streetStr = $('#street').val();
+    // let cityStr = $('#city').val();
+    // let address = streetStr + ','+ cityStr
+    // let imgSize = '600x300'
+    //
+    // $greeting.text(`So you are planning to explore ${address} ?`)
+    //
+    // let streetViewUrl =
+    // 'https://maps.googleapis.com/maps/api/streetview?size=600x300&location='
+    // + address
+    // +'&key=' + config.MapApiKey;
+    //
+    // $body.append(
+    //   `<img class="bgimg" src="${streetViewUrl}" >`
+    // )
+
+    // NYT Articles
+    var url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
+    url += cityName + '&sort=newest&api-key=' + nyTimesAPIKey;
+    $.getJSON(url, function(data){
+        $nytHeaderElem.text('New York Times Articles About ' + cityName);
+
+        articles = data.response.docs;
+        for (var i=0; i < articles.length; i++) {
+            var article = articles[i];
+            $nytElem.append('<li class="articles">' +
+                '<a href="' + article.web_url + '" target="_blank">' + article.headline.main + '</a>' +
+                '<p>' + article.snippet + '</p>' +
+                '</li>');
+        };
+
+    }).fail(function(){
+        $nytHeaderElem.text('There was an error loading the NY Times Article');
+
+    });
+
 
     return false;
 };
 
 $('#form-container').submit(loadData);
-
-// loadData();
