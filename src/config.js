f542a6ffe30a7a51dad3a07693eb78d669d38666
elfi yang <elfiyang16@gmail.com>
2019-12-23 15:45:30 +0000

    fix: add config file to show api

---
+++ src/config.js
@@ -0,0 +1,6 @@
+const config = {
+  weatherApi: '3c3eaa1b91433ff990e7d5b185410ab2',
+  guardianApi : 'bb72f44a-b9d7-4d8d-90e3-af4717a1e8d0',
+  bingMapApi: 'AqIttSRDn7kblpvTiIjolo7B5ZVK764G35GTSJp0-O15LDwwXnhk5egh_GDzjtgC'
+
+}
